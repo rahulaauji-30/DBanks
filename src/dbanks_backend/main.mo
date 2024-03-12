e7848789c0e278964a1c2b67dfa705c12b72e555
Rahul Aauji <rahulaauji71@gmail.com>
2024-03-12 23:57:05 +0530

    Final commit

--- src/dbanks_backend/main.mo
+++ src/dbanks_backend/main.mo
@@ -1,5 +1,19 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Float "mo:base/Float";
+import Debug "mo:base/Debug";
+actor DBank{
+  stable var currentBalance:Float = 400;
+
+  public query func checkBalance():async Float{
+    return currentBalance;
   };
-};
+  public func topUp(amount:Float){
+    currentBalance += amount;
+  };
+  public func withDraw(amount:Float){
+    if(currentBalance - amount >= 0){
+      currentBalance-=amount;
+    }else{
+      Debug.print("Insufficient Balance");
+    }
+  }
+}
--- src/dbanks_frontend/src/App.jsx
+++ src/dbanks_frontend/src/App.jsx
@@ -1,29 +1,81 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { dbanks_backend } from 'declarations/dbanks_backend';
-
+import fav from "../public/favicon.ico"
 function App() {
   const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    dbanks_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
+  const [balance,setBalance] = useState(100);
+  const [isLoading,setLoading] = useState(false)
+  const [info,setInfo] = useState({
+    name:"",
+    amount:0
+  })
+  const fetchBalance = async () => {
+    setLoading(true);
+    try {
+      const updatedBalance = await dbanks_backend.checkBalance();
+      setBalance(updatedBalance);
+    } catch (error) {
+      console.error("Error fetching balance:", error); // Handle errors gracefully
+    } finally {
+      setLoading(false);
+    }
   }
-
+  function handleChange(event){
+    const {name,value} = event.target;
+    
+    const info = {
+      name:name,
+      amount:value
+    }
+    setInfo(info)
+  }
+  async function handleSubmit(event){
+    event.preventDefault();
+    setLoading(true)
+    const amount = parseFloat(info.amount)
+    try{
+      if(info.amount!=0){
+        if(info.name === "topup"){
+          try{
+           await dbanks_backend.topUp(amount);
+           console.log("Added successfully");
+          }catch(err){
+           console.log(err);
+          }
+         }else if(info.name==="withdraw"){
+           try{
+             await dbanks_backend.withDraw(amount);
+             
+             console.log("Withdrawl");
+           }catch(err){
+             console.log(err);
+           }
+         }
+      }
+    }catch(err){
+      console.log(err);
+    }finally{
+      fetchBalance()
+      setLoading(false)
+    }
+  }
+  useEffect(() => {
+    fetchBalance();
+  }, []);
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <div class="main">
+        <img  src={fav} width="100px" height="100px"/>
+        <p id="balance">Current Balance : ${balance}</p>
+        <hr/>
+        <form onSubmit={handleSubmit}>
+          <label for="">Amount to Top Up</label>
+          <input type="text" name="topup" id="" onChange={handleChange}/>
+          <label for="">Amount to withdrawl</label>
+          <input type="text" name="withdraw" id="" onChange={handleChange}/>
+          <input type="submit" disabled={isLoading} value="Finalise Transaction" />
+        </form>
+      </div>
     </main>
   );
 }
